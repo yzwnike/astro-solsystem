d90e62dad9c714a3b06a741a3c6385bbe91fba0f
yzwnike <nicovehi@gmail.com>
2026-07-07 18:53:02 +0200

    v2.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/legalizacion-electrica/',
+  // Eliminamos el base global para poder usar múltiples landings independientes
   build: {
     assetsPrefix: 'https://lanzadera-digital.b-cdn.net/solsystems.es'
   }
